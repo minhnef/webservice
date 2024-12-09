abac16909327bd426c0fe2784ede9d2bb4d9ee8f
minhnef <142522727+minhnef@users.noreply.github.com>
2024-12-09 10:26:22 +0700

    mn code vao day nhe

---
+++ webservice/nhom10/src/test/java/com/example/webservice/nhom10/Nhom10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservice.nhom10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
