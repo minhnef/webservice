988bf6c6afd19d47c91fc36ddd931c5b23ba876b
minhnef <142522727+minhnef@users.noreply.github.com>
2024-12-09 10:26:22 +0700

    mn code vao day nhe

---
+++ webservice/nhom10/src/main/java/com/example/webservice/nhom10/Nhom10Application.java
@@ -0,0 +1,13 @@
+package com.example.webservice.nhom10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom10Application.class, args);
+	}
+
+}
