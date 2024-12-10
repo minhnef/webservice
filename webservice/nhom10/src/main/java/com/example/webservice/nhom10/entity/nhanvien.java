e308e1fad0fa7b2040d5b36d1c811bc0d4c7c0b4
minhnef <142522727+minhnef@users.noreply.github.com>
2024-12-10 13:44:43 +0700

    phân chia thu mục cần có

---
+++ webservice/nhom10/src/main/java/com/example/webservice/nhom10/entity/nhanvien.java
@@ -0,0 +1,27 @@
+package com.example.webservice.nhom10.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class nhanvien {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int idnhanvien;
+    
+
+}
