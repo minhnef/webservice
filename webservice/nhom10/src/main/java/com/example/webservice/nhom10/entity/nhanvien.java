deb8cff6c02e55acf8f5b7e692869dc765aa845a
minhnef <142522727+minhnef@users.noreply.github.com>
2024-12-11 21:42:10 +0700

    Add files via upload

--- webservice/nhom10/src/main/java/com/example/webservice/nhom10/entity/nhanvien.java
+++ webservice/nhom10/src/main/java/com/example/webservice/nhom10/entity/nhanvien.java
@@ -1,20 +1,26 @@
 package com.example.webservice.nhom10.entity;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Entity
 @Table
-@Setter
-@Getter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class nhanvien {
@@ -22,6 +28,23 @@ public class nhanvien {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int idnhanvien;
-    
+    @Column
+    private String hoten;
+    @Column
+    private LocalDate ngaysinh;
+    @Column
+    private String diachi;
+    @jakarta.validation.constraints.Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Số điện thoại không hợp lệ")
+    @Column
+    private String sdt;    
+    @Column
+    private String lichlamviec;
+    @Column
+    private float luong;
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JsonIgnore
+    private taikhoan taikhoan_nv;
+
 
 }
--- webservice/nhom10/src/main/java/com/example/webservice/nhom10/entity/taikhoan.java
+++ webservice/nhom10/src/main/java/com/example/webservice/nhom10/entity/taikhoan.java
@@ -1,22 +1,21 @@
 package com.example.webservice.nhom10.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class taikhoan {
@@ -35,9 +34,12 @@ public class taikhoan {
     @Column
     private boolean trangthai;
 
-    @OneToOne
-    @JoinColumn(name = "idquanly")
+    @OneToOne(mappedBy = "taikhoan_ql", cascade = CascadeType.ALL, orphanRemoval = true)
+    @PrimaryKeyJoinColumn
     private quanly quanly;
 
+    @OneToOne(mappedBy = "taikhoan_nv", cascade = CascadeType.ALL, orphanRemoval = true)
+    @PrimaryKeyJoinColumn
+    private nhanvien nhanvien;
 
 }
