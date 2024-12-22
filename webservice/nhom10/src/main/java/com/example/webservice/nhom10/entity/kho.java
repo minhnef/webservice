2118c6512e3b5ae4a359571e6d745e561a55d1f5
dmToan <dowis.naot@gmail.com>
2024-12-22 21:43:17 +0700

    Kho

--- webservice/nhom10/src/main/java/com/example/webservice/nhom10/entity/kho.java
+++ webservice/nhom10/src/main/java/com/example/webservice/nhom10/entity/kho.java
@@ -16,6 +16,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -24,22 +25,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class kho {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int id;
+    @Getter
     @Column
     private String tennguyenlieu;
+    @Getter
     @Column
     private int gia;
+    @Getter
     @Column
     private String noinhap;
+    @Getter
     @Column
     private LocalDate ngaynhap;
+    @Getter
     @Column
     private int soluong;
+    @Getter
     @Column
     private String donvido;
+    @Getter
     @Column
     private int tongtien;
 
@@ -50,4 +59,67 @@ public class kho {
     @JoinColumn(name = "idquanly")
     private quanly idquanly;
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTennguyenlieu(String tennguyenlieu) {
+        this.tennguyenlieu = tennguyenlieu;
+    }
+
+    public void setGia(int gia) {
+        this.gia = gia;
+    }
+
+    public void setNoinhap(String noinhap) {
+        this.noinhap = noinhap;
+    }
+
+    public void setNgaynhap(LocalDate ngaynhap) {
+        this.ngaynhap = ngaynhap;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+
+    public void setDonvido(String donvido) {
+        this.donvido = donvido;
+    }
+
+    public void setTongtien(int tongtien) {
+        this.tongtien = tongtien;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTennguyenlieu() {
+        return tennguyenlieu;
+    }
+
+    public int getGia() {
+        return gia;
+    }
+
+    public String getNoinhap() {
+        return noinhap;
+    }
+
+    public LocalDate getNgaynhap() {
+        return ngaynhap;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public String getDonvido() {
+        return donvido;
+    }
+
+    public int getTongtien() {
+        return tongtien;
+    }
 }
