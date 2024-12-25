2fc3daa94174717b9a4fc995e369796dda504101
minhnef <minhhnim134@gmail.com>
2024-12-25 11:33:04 +0700

    chinh sua lai khorepo

--- webservice/nhom10/src/main/java/com/example/webservice/nhom10/repository/khorepo.java
+++ webservice/nhom10/src/main/java/com/example/webservice/nhom10/repository/khorepo.java
@@ -1,9 +1,11 @@
 package com.example.webservice.nhom10.repository;
 
-import com.example.webservice.nhom10.entity.kho;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.webservice.nhom10.entity.kho;
+
 @Repository
-public interface khorepo extends JpaRepository<kho,Integer> {
+public interface KhoRepo extends JpaRepository<kho, Integer>{
+
 }
